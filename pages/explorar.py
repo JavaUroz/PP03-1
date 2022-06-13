b4d9d638a23cd6935e38293492115057222c9177
Javier Uroz <urozjavier@gmail.com>
2022-06-13 12:37:58 -0300

    v2

--- pages/explorar.py
+++ pages/explorar.py
@@ -1,16 +1,16 @@
-from types import MappingProxyType
 import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 
 mpg = sns.load_dataset('mpg')
 registros = len(mpg)
 atributos = mpg.shape
 tipos = mpg.dtypes.astype(str)
 estadistico = mpg.describe()
-categorias = mpg.species.unique()
-g = sns.pairplot(mpg, hue='species')
+categorias = mpg.origin.unique()
+g = sns.pairplot(mpg, hue='origin')
 
 def show_explore_page():
     st.title("Explorador de la fuente de  datos")
